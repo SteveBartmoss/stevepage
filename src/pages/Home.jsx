b069f1b1fe3678fc16d5a540519006410b670c74
Marco <za19012397@zapopan.tecmm.edu.mx>
2025-07-18 20:39:37 -0600

    feat: Se agrego una pagina para el home

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+
+
+export function Home() {
+    return (
+        <>  
+            <h1>Hi stalker</h1>
+            <section>
+                <p>
+                    Hola esto es una pagina web
+                </p>
+            </section>
+        </>
+    )
+}
